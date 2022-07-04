5b17acffe085e5377aa69d1082f4b65ad69b2563
nazaninsnr <103143082+nazaninsnr@users.noreply.github.com>
2022-07-04 17:25:43 +0430

    Added Computer Choice Structure

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const yourChoiceDisplay = document.getElementById("your-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
+let computerChoice;
 
 possibleChoices.forEach((possibleChoices) =>
   possibleChoices.addEventListener("click", (event) => {
     userChoice = event.target.id;
     yourChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
   })
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+  if (randomNumber === 1) computerChoice = "rock";
+  if (randomNumber === 2) computerChoice = "scissor";
+  if (randomNumber === 3) computerChoice = "paper";
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
